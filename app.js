bec15574d9fdc8be9266268364c93132ed10276c
Hüseyin Ensari <hsynensari@gmail.com>
2021-11-06 04:19:01 +0300

    app.js settings have been made

---
+++ app.js
@@ -0,0 +1,19 @@
+// npm imports
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+// file imports
+const indexRouter = require('./src/routers/indexRouter');
+// database
+require('./src/configs/dbConnection');
+// app settings and middlewares
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', path.resolve(__dirname, '/src/views'));
+app.use('/img', express.static(path.join(__dirname, '/src/uploads')));
+// router
+app.use('/', indexRouter);
+// listen
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(PORT, ' is active'));
---
+++ src/configs/dbConnection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const { MONGO_URL } = process.env;
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB CONNECTED'))
+  .catch((err) => console.log('DB connection ERROR -> ', err));
